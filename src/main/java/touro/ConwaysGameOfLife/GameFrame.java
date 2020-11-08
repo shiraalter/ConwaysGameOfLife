8a4bb679e83ea015ab191084e837a8fefc0dca2f
Devora Nagel <devoranagel@gmail.com>
2020-11-08 12:16:43 -0600

    working on gameFrame

--- src/main/java/touro/ConwaysGameOfLife/GameFrame.java
+++ src/main/java/touro/ConwaysGameOfLife/GameFrame.java
@@ -4,21 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameFrame extends JFrame {
+    Grid grid;
 
     private JPanel panel;
     private JButton clear;
     private JButton play;
     private JButton next;
 
-    public GameFrame(GridView gridView, Grid grid, GridMouseListener mouseListener) {
+    public GameFrame(GridView gridView, GridMouseListener mouseListener) {
         setSize(1000, 400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Game of Life");
         setLayout(new BorderLayout());
 
         clear = new JButton("Clear");
+        clear.addActionListener(actionEvent -> reset());
         play = new JButton("Play");
+        play.addActionListener(actionEvent -> playGame());
         next = new JButton("Next >");
+        next.addActionListener(actionEvent -> nextMove());
         panel = new JPanel(new FlowLayout());
         panel.add(clear);
         panel.add(play);
@@ -27,5 +31,19 @@ public class GameFrame extends JFrame {
         add(gridView, BorderLayout.CENTER);
         addMouseListener(mouseListener);
         add(panel, BorderLayout.SOUTH);
+    }
+
+    private void nextMove() {
+        //does this make continuous moves or a single move?
+        //grid.makeMove();
+    }
+
+    private void playGame() {
+        //does this make continuous moves or a single move?
+        //grid.makeMove();
+    }
+
+    private void reset() {
+        grid.setDead(getX(),getY());
     }
 }
