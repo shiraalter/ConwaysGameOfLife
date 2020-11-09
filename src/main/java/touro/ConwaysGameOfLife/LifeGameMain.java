6e131ba3acb7e62a08d0aef7e6656ab802f79e90
shiraalter <shiraalter@gmail.com>
2020-11-08 21:49:28 -0500

    added main

--- src/main/java/touro/ConwaysGameOfLife/LifeGameMain.java
+++ src/main/java/touro/ConwaysGameOfLife/LifeGameMain.java
@@ -4,9 +4,7 @@ public class LifeGameMain {
 
     public static void main(String[] args) {
         //All the things we need
-        int HEIGHT = 20;
-        int WIDTH = 20;
-        Grid grid = new Grid(WIDTH, HEIGHT);
+        Grid grid = new Grid();
         GridView gridView = new GridView(grid);
         new GameFrame(gridView, grid).setVisible(true);
 
