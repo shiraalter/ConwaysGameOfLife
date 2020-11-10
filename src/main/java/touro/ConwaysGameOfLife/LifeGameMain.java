8b88025186e4ba3fec3a4e83c66b157ea728ed57
shiraalter <shiraalter@gmail.com>
2020-11-10 09:26:48 -0500

    removed comment

--- src/main/java/touro/ConwaysGameOfLife/LifeGameMain.java
+++ src/main/java/touro/ConwaysGameOfLife/LifeGameMain.java
@@ -2,7 +2,7 @@ package touro.ConwaysGameOfLife;
 
 public class LifeGameMain {
     public static void main(String[] args) {
-        //All the things we need
+
         Grid grid = new Grid();
         GridView gridView = new GridView(grid);
         new GameFrame(gridView, grid).setVisible(true);
